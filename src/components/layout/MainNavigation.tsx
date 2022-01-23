a8ca15ea3bd42c209a74b5cfa71a04a7e574face
Hani <h.samir@edu.salford.ac.uk>
2022-01-23 15:32:00 +0000

    project finished, added counter for favourites

--- src/components/layout/MainNavigation.tsx
+++ src/components/layout/MainNavigation.tsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import {Link} from 'react-router-dom';
+import FavouritesContext from '../../store/favourites-context';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
+    const favouritesContext = useContext(FavouritesContext);
     return (
     <header className={classes.header}>
         <div className={classes.logo}>React Meetups</div>
@@ -9,7 +12,7 @@ function MainNavigation() {
             <ul>
                 <li><Link to='/'>All Meetups</Link></li>
                 <li><Link to='/new-meetup'>New Meetups</Link></li>
-                <li><Link to='/favourites'>Favourites</Link></li>
+                <li><Link to='/favourites'>Favourites <span className={classes.badge}>{favouritesContext.totalFavourites}</span></Link></li>
             </ul>
         </nav>
     </header>
